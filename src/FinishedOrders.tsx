4e8d5cdbe4bdccb52483e07034b72c051e1e6202
Alvinotuya84 <alvinotuya8@gmail.com>
2023-01-24 19:35:04 +0300

    printing report

--- src/FinishedOrders.tsx
+++ src/FinishedOrders.tsx
@@ -8,7 +8,8 @@ import io from 'socket.io-client';
 import {  toast } from 'react-toastify';
 import { format } from 'date-fns';
 import Swal from 'sweetalert2'
-
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
 
 function FinishedOrders() {
@@ -32,6 +33,36 @@ function FinishedOrders() {
 
 
   const socket = io('http://localhost:3001');
+
+  const printReport=()=>{
+    const unit = "pt";
+    const size = "A5";// Use A1, A2, A3 or A4
+    const orientation = "landscape";
+
+    const marginLeft = 10;
+    const doc = new jsPDF(orientation, unit, size);
+
+    doc.setFontSize(17);
+
+    const title = "Orders Report";
+    const headers = [["ORDER ID", "TOPPINGS","DOUGH TIME","TOPPING TIME","OVEN TIME","WALK TIME","COMPLETED TIME"]];
+
+    const data = allOrders.map(elt=> [elt._id, elt.toppings.join(', '),
+    elt.doughPrepTime,elt.toppingPrepTime,elt.ovenPrepTime,elt.walkTime,
+    format(new Date(elt.timeCompleted), "hh:mm:ss aa")
+  ]);
+
+    let content = {
+      startY: 50,
+      head: headers,
+      body: data,
+      rowHeight:30
+    };
+    doc.setTextColor("#1683d8")
+    doc.text(doc.internal.pageSize.getWidth()/2,20,title ,null,null, "center");
+    doc.autoTable(content);
+    doc.save("report.pdf")
+  }
 
 
 
@@ -82,25 +113,42 @@ socket.on('orderCompleted', (orders,orderId) => {
 
   return (
     <div className="relative overflow-x-auto">
+      <div className="flex justify-between w-full">
+            <button onClick={()=>{
+
+      Swal.fire({
+      title: 'Are you sure you want to delete all orders?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+      if (result.isConfirmed) {
+      setIsLoading(true)
+      dispatch(clearAllOrders())
+      }
+      })
+
+      }} disabled={isLoading} type="button" className="text-white bg-gradient-to-r
+      from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br
+      focus:ring-4 focus:outline-none focus:ring-red-300
+      dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg
+      dark:shadow-red-800/80 font-medium rounded-lg
+      text-sm px-5 py-2.5 text-center mr-2 mb-2">{isLoading?'loading...':'Clear All'}
+      </button>
       <button onClick={()=>{
-
-        Swal.fire({
-  title: 'Are you sure you want to delete all orders?',
-  text: "You won't be able to revert this!",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonColor: '#3085d6',
-  cancelButtonColor: '#d33',
-  confirmButtonText: 'Yes, delete it!'
-}).then((result) => {
-  if (result.isConfirmed) {
-    setIsLoading(true)
-    dispatch(clearAllOrders())
-  }
-})
-
-      }} disabled={isLoading} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">{isLoading?'loading...':'Clear All'}</button>
-
+        printReport()
+}}  type="button" className="text-white
+bg-gradient-to-r from-green-400 gray-pink-500
+ to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-gray-300
+  dark:focus:ring-green-800 shadow-lg shadow-gray-500/50 dark:shadow-lg
+   dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5
+    py-2.5 text-center mr-2 mb-2">
+      Print Report
+      </button>
+      </div>
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
